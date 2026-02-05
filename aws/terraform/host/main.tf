4c723aab00d91b64843111b04296afd33ec2d30f
vickmp <vpalma@opennebula.io>
2026-02-05 16:12:14 +0100

    F OpenNebula/one#7460: Add AWS driver

---
+++ aws/terraform/host/main.tf
@@ -0,0 +1,30 @@
+resource "aws_key_pair" "oneadmin_pubkey" {
+    key_name   = "provision_${var.provision_id}_key_${var.key_suffix}"
+    public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
+}
+
+resource "aws_instance" "host" {
+    ami           = lookup(local.registred_instance_os_name, var.instance_os_name)
+    instance_type = var.instance_type
+    count         = var.oneform_hosts
+
+    vpc_security_group_ids = [var.vpc_security_group_id]
+    subnet_id              = var.vpc_subnet_id
+
+    root_block_device {
+        volume_size = var.instance_disk_size
+    }
+
+    key_name = aws_key_pair.oneadmin_pubkey.key_name
+
+    tags = merge(
+        var.aws_tags,
+        {
+            Name = "provision_${var.provision_id}_host_${count.index}"
+        }
+    )
+
+    timeouts {
+        delete = "30m"
+    }
+}
