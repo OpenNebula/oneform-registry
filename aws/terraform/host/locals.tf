41102413fca35f5b12e007fb5ca8941c71d73036
vickmp <vpalma@opennebula.io>
2026-02-17 16:56:40 +0100

    M #-: Remove unused local

--- aws/terraform/host/locals.tf
+++ aws/terraform/host/locals.tf
@@ -1,10 +1,3 @@
-resource "random_id" "key_suffix" {
-    byte_length = 6
-    keepers = {
-        provision_id = var.provision_id
-    }
-}
-
 locals {
 	registred_instance_os_name = {
 		"ubuntu_2204" = data.aws_ami.ubuntu_2204.id
